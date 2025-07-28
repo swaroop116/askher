b94e4ae29e9b08f9bab6ddf55c45b544b9947744
Sai Swaroop Rachakonda <71523052+swaroop116@users.noreply.github.com>
2025-07-29 00:35:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,14 +39,13 @@ function createHeart() {
 
 // Confetti from CDN
 function confetti() {
-  const script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js';
-  script.onload = () => {
-    window.confetti({
-      particleCount: 150,
-      spread: 80,
-      origin: { y: 0.6 },
-    });
+  window.confetti({
+    particleCount: 200,
+    spread: 100,
+    origin: { y: 0.6 },
+  });
+}
+
   };
   document.body.appendChild(script);
 }
